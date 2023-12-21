a7d1646fc262f593c2cea07c83d17bb3e2c23a21
EricAlejandroDurante <eric.alejandro.durante@gmail.com>
2023-12-21 02:16:09 -0300

    first commit

---
+++ presente/src/draw.h
@@ -0,0 +1,16 @@
+#ifndef DRAW_H
+#define DRAW_H
+
+#include "level.h"
+#include "state.h"
+
+// Draws the cells of the level in the screen
+void draw_level(const level *lvl);
+
+// Draws the whole state and the level in the screen
+void draw_state(const level *lvl, const state *sta);
+
+// Dibujar el menu de armas
+void DrawWeapons(int buttonR, int buttonY, int buttonT);
+
+#endif
